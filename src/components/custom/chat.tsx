0f72ec8347ecd2ac1a6db155a3dcf808f2626365
Rexes Murro <john.murro@gridapp.io>
2025-09-08 21:09:09 +0800

    Add intro message to Deep Chat

--- src/components/custom/chat.tsx
+++ src/components/custom/chat.tsx
@@ -8,10 +8,13 @@ const ChatWithAI = () => {
     const [showChat, setShowChat] = React.useState(false);
     return (
         <div className='chat'>
-            {showChat && <DeepChat connect={{
-                url: "https://api.rexes.cloud/api/chat",
-                method: "POST",
-            }} />}
+            {showChat &&
+                <DeepChat
+                    introMessage={{ text: 'Hi! I am Rexes. How can I help you?' }}
+                    connect={{
+                        url: "https://api.rexes.cloud/api/chat",
+                        method: "POST",
+                    }} />}
             <Button variant={'outline'} size={'lg'} onClick={() => setShowChat(!showChat)}>
                 <MessageSquareIcon width={40} height={40} onClick={() => setShowChat(!showChat)} />
                 Chat
